18ebc5381e33c06eedc84f3b5cf8dcac1911df14
techbase2brand <techbase2brand@gmail.com>
2025-11-19 19:24:23 +0530

    upadtes in popup and add from in promo page manpreet sir changes and add meta pixel code

--- src/app/Metalinksseo.jsx
+++ src/app/Metalinksseo.jsx
@@ -64,6 +64,36 @@ export default function Metalinksseo() {
         />
       </noscript>
 
+      {/* ===== Meta Pixel 3 ===== */}
+      <Script
+        id="meta-pixel-3"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '25867542746180452');
+          fbq('track', 'PageView');
+        `,
+        }}
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=25867542746180452&ev=PageView&noscript=1"
+          alt="meta-pixel-3"
+        />
+      </noscript>
+
       {/* ===== Website Schema ===== */}
       <Script
         id="schema-website"
