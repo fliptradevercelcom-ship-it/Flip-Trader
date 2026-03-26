953ba7035f784ecbdca3c8dcaad342b277ef906c
techbase2brand <techbase2brand@gmail.com>
2026-03-26 18:43:22 +0530

    disable lightningcss and fonts

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   experimental: {
+//     optimizeCss: false,
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
     optimizeCss: false,
   },
+  compiler: {
+    removeConsole: false,
+  },
+  // 👇 ye add karo
+  optimizeFonts: false,
 };
 
 export default nextConfig;
